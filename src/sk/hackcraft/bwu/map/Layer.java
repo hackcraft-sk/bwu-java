47a47026dd2279cc1db376f3d04045e65d9dbf8a
Matej Isteník <matej.istenik@gmail.com>
2014-06-03 14:15:54 +0200

    Finished map layer class and added tests for it

--- src/sk/hackcraft/bwu/map/Layer.java
+++ src/sk/hackcraft/bwu/map/Layer.java
@@ -13,6 +13,42 @@ public class Layer
 		this.matrix = new int[width * height];
 	}
 	
+	public Layer(Layer layer, int offsetX, int lengthX, int offsetY, int lengthY)
+	{
+		this(lengthX, lengthY);
+		
+		int thisX = 0, thisY = 0;
+		
+		for (int x = 0; x < layer.width; x++)
+		{
+			if (x < offsetX || x >= offsetX + lengthX)
+			{
+				continue;
+			}
+			
+			for (int y = 0; y < layer.height; y++)
+			{
+				if (y < offsetY || y >= offsetY + lengthY)
+				{
+					continue;
+				}
+				
+				int value = layer.get(x, y);
+				set(thisX, thisY, value);
+				
+				thisY++;
+			}
+			
+			thisX++;
+			thisY = 0;
+		}
+	}
+	
+	public Layer(Layer layer)
+	{
+		this(layer, 0, layer.width, 0, layer.height);
+	}
+	
 	public int getWidth()
 	{
 		return width;
@@ -25,13 +61,24 @@ public class Layer
 	
 	public int get(int x, int y)
 	{
-		int i = y * height + x;
+		int i = positionToIndex(x, y);
 		return matrix[i];
 	}
 	
 	public void set(int x, int y, int value)
 	{
-		int i = y * height + x;
+		int i = positionToIndex(x, y);
 		matrix[i] = value;
+	}
+	
+	private int positionToIndex(int x, int y)
+	{
+		return  y * width + x;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return String.format("Layer %dx%d", width, height);
 	}
 }
